c93e4dab0e11f57ac76437f41c4bb5daf585c3b2
elfarsif <f.n.elfarsi@gmail.com>
2024-02-19 17:49:56 -0500

    added click for color

--- script.js
+++ script.js
@@ -7,7 +7,22 @@ document.addEventListener('DOMContentLoaded', ()=>{
 
     for(let i=0;i<squares.length;i++){
         squares[i].onclick=()=>{
-            alert('clicked the square'+i)
+            //if square below you current quare is taken u can go on top of it
+            if(squares[i+7].classList.contains('taken')){
+                if(currentPlayer==1){
+                    squares[i].classList.add('taken')
+                    squares[i].classList.add('player-one')
+                    currentPlayer=2;
+                    displayCurrentPlayer.innerHTML=currentPlayer
+                }else if(currentPlayer==2){
+                    squares[i].classList.add('taken')
+                    squares[i].classList.add('player-two')
+                    currentPlayer=1
+                    displayCurrentPlayer.innerHTML=currentPlayer
+                }
+            }else{
+                    alert('cant go here')
+            }
         }
     }
 
